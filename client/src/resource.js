c06c24adc3310ae8fe3da05b6d7e5676d48098ed
Chris Hannon <himynameschris@gmail.com>
2012-08-12 15:26:34 -0400

    task: add cocos2d-html5 template and update references

---
+++ client/src/resource.js
@@ -0,0 +1,20 @@
+var s_HelloWorld = "/client/res/HelloWorld.png";
+var s_CloseNormal = "/client/res/CloseNormal.png";
+var s_CloseSelected = "/client/res/CloseSelected.png";
+
+var g_ressources = [
+    //image
+    {type:"image", src:s_HelloWorld},
+    {type:"image", src:s_CloseNormal},
+    {type:"image", src:s_CloseSelected}
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
