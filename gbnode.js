996504bd828511ac2499ed26c89a466dd2b6e5e2
Chris Hannon <himynameschris@gmail.com>
2012-08-12 14:39:29 -0400

    Refactor: change directory structure and prepare for standalone lib

--- gbnode.js
+++ gbnode.js
@@ -19,27 +19,52 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  ****************************************************************************/
+var Box2D = require('./lib/box2d.js');
 
-/**
- implementing the gbnode class, its purpose is to manage information necc to communicate sprite details to the client
+// Shorthand "imports"
+var b2Vec2 = Box2D.Common.Math.b2Vec2,
+    b2BodyDef = Box2D.Dynamics.b2BodyDef,
+    b2AABB = Box2D.Collision.b2AABB,
+    b2Body = Box2D.Dynamics.b2Body,
+    b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
+    b2Fixture = Box2D.Dynamics.b2Fixture,
+    b2World = Box2D.Dynamics.b2World,
+    b2MassData = Box2D.Collision.Shapes.b2MassData,
+    b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+    b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+    b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
+    b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef,
+    b2EdgeShape = Box2D.Collision.Shapes.b2EdgeShape;
+b2ContactListener = Box2D.Dynamics.b2ContactListener;
 
- */
-var gbnode = function() {
-    gbnode.init();
-};
+(function(){
 
-/**
- init the gbnode, setting members to defaults
+    GBox2D.namespace("GBox2D.GBNode");
 
- */
-gbnode.init = function() {
+    /**
+     implementing the gbnode class, its purpose is to manage information necc to communicate sprite details to the client
 
-};
+     */
+    GBox2D.GBNode = function() {
+        this.init();
+    };
 
-/**
- static method to create a gbnode from a name, type and frame
+    GBox2D.GBNode.prototype = {
 
- */
-gbnode.create = function(name, type, frame) {
+        /**
+         init the gbnode, setting members to defaults
 
-};
+         */
+        init : function() {
+
+        },
+
+        /**
+         static method to create a gbnode from a name, type and frame
+
+         */
+        create : function(name, type, frame) {
+
+        }
+    };
+})();
