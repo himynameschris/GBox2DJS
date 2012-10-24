46eb72a5d917370c90418ad49249fae9fb6ed6e5
Chris Hannon <himynameschris@gmail.com>
2012-10-24 17:17:10 -0400

    task: update format for express route

--- demo/server/routes/index.js
+++ demo/server/routes/index.js
@@ -4,5 +4,11 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', {title: 'GBox2DJS Demo', vars: {message: 'A Simple Demo', navbar: 'GBox2DJS Demo' }})
+  res.render('index', {
+      title: 'GBox2DJS Demo',
+       vars: {
+           message: 'A Simple Demo',
+           navbar: 'GBox2DJS Demo'
+       }
+  })
 };
