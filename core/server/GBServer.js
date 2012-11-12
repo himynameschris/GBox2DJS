240403e0a0327a608659ec5ea75a6a5bef4a2a60
Chris Hannon <himynameschris@gmail.com>
2012-11-12 13:06:41 -0500

    task: implement login with passport and session management with express

--- core/server/GBServer.js
+++ core/server/GBServer.js
@@ -47,21 +47,113 @@
                 server = express(),
                 iolib = require('socket.io');
 
-            if(this.routePath != null) {
-                this.routes = require(this.routePath);
-                server.get('/', this.routes.index);
+
+            var users = [
+                { id: 1, username: 'test', password: 'secret2', email: 'test@x.com' },
+                { id: 2, username: 'test2', password: 'secret2', email: 'test@x.com' },
+                { id: 3, username: 'test3', password: 'secret2', email: 'test@x.com' }
+            ];
+
+            function findById(id, fn) {
+                var idx = id - 1;
+                if (users[idx]) {
+                    fn(null, users[idx]);
+                } else {
+                    fn(new Error('User ' + id + ' does not exist'));
+                }
             }
+
+            function findByUsername(username, fn) {
+                for (var i = 0, len = users.length; i < len; i++) {
+                    var user = users[i];
+                    if (user.username === username) {
+                        return fn(null, user);
+                    }
+                }
+                return fn(null, null);
+            }
+
+            function checkAuth(req,res,next){
+                if(req.user) next();//Forward the request so it can be handled by your router
+                else res.redirect('/login');//res.send(403);//send access denied
+            }
+
+            // Passport session setup.
+            //   To support persistent login sessions, Passport needs to be able to
+            //   serialize users into and deserialize users out of the session.  Typically,
+            //   this will be as simple as storing the user ID when serializing, and finding
+            //   the user by ID when deserializing.
+            passport.serializeUser(function(user, done) {
+                done(null, user.id);
+            });
+
+            passport.deserializeUser(function(id, done) {
+                findById(id, function (err, user) {
+                    done(err, user);
+                });
+            });
+
+            // Use the LocalStrategy within Passport.
+            //   Strategies in passport require a `verify` function, which accept
+            //   credentials (in this case, a username and password), and invoke a callback
+            //   with a user object.  In the real world, this would query a database;
+            //   however, in this example we are using a baked-in set of users.
+            passport.use(new LocalStrategy(
+                function(username, password, done) {
+                    // asynchronous verification, for effect...
+                    process.nextTick(function () {
+
+                        // Find the user by username.  If there is no user with the given
+                        // username, or the password is not correct, set the user to `false` to
+                        // indicate failure and set a flash message.  Otherwise, return the
+                        // authenticated `user`.
+                        findByUsername(username, function(err, user) {
+                            if (err) { return done(err); }
+                            if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+                            if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+                            return done(null, user);
+                        })
+                    });
+                }
+            ));
 
             var viewPath = this.viewPath;
             server.configure(function() {
-                console.log("dirname: " + __dirname + " viewpath: " + viewPath);
                 server.set('views', viewPath);
                 server.set('view engine', 'jade');
                 server.set('view options', {layout: false});
+                //server.use(express.logger());
+                server.use(express.cookieParser());
                 server.use(express.bodyParser());
-                //that.server.use(that.express.methodOverride());
+                //server.use(express.session({secret:'keyboard cat'}));
+                server.use(express.cookieSession({secret:"thesecret"}));
+
+                // Initialize Passport!  Also use passport.session() middleware, to support
+                // persistent login sessions (recommended).
+                server.use(passport.initialize());
+                server.use(passport.session());
+
                 server.use(server.router);
                 server.use(express.static(__dirname + '/public'));
+                server.use('/', express.static(__dirname + '/../../') );
+            });
+
+            if(this.routePath != null) {
+                this.routes = require(this.routePath);
+
+                server.get('/login', this.routes.login);
+
+                server.post('/login',
+                    passport.authenticate('local', { successRedirect: '/',
+                        failureRedirect: '/login'})
+                );
+
+                server.get('/', checkAuth, this.routes.index);
+
+            }
+
+            server.get('/api/hello', function(req,res){
+                res.send('Hello World');
             });
 
             var app = http.createServer(server).listen(GBox2D.constants.GBServerNet.SERVER_PORT);
@@ -69,11 +161,7 @@
             var io = iolib.listen(app);
             io.set("log level", 0);
 
-            server.use('/', express.static(__dirname + '/../../') );
 
-            server.get('/api/hello', function(req,res){
-                res.send('Hello World');
-            });
 
             var that = this;
             io.on('connection', function (client) {
--- demo/server/routes/index.js
+++ demo/server/routes/index.js
@@ -7,8 +7,19 @@ exports.index = function(req, res){
   res.render('index', {
       title: 'GBox2DJS Demo',
        vars: {
-           message: 'A Simple Demo',
+           message: "Welcome " + req.session.passport.user + "!",
            navbar: 'GBox2DJS Demo'
        }
   })
+};
+
+
+exports.login = function(req,res){
+    res.render('login', {
+        title: 'GBox2DJS Demo',
+        vars: {
+            message: '',
+            navbar: 'GBox2DJS Demo'
+        }
+    });
 };
