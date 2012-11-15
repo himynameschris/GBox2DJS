30ab7095295b0dc44f3bc2886f49fad0b61332b7
Chris Hannon <himynameschris@gmail.com>
2012-11-15 18:53:11 -0500

    task: move routes into individual module and create routes for game template

--- demo/server/routes/index.js
+++ demo/server/routes/index.js
@@ -1,25 +1,57 @@
 
-/*
- * GET home page.
- */
+module.exports = {
 
-exports.index = function(req, res){
-  res.render('index', {
-      title: 'GBox2DJS Demo',
-       vars: {
-           message: "Welcome " + req.session.passport.user + "!",
-           navbar: 'GBox2DJS Demo'
-       }
-  })
-};
+    index : function(req, res){
 
+        var gameTable = [];
 
-exports.login = function(req,res){
-    res.render('login', {
-        title: 'GBox2DJS Demo',
-        vars: {
-            message: '',
-            navbar: 'GBox2DJS Demo'
-        }
-    });
+        var i = 0;
+
+        req.server.games.forEach( function(key, game){
+            gameTable[i] = game;
+            i++;
+        }, this );
+
+        GBox2D.constants.findById(req.session.passport.user, function(v1, v2) {
+            console.log("v1: " + v1 + " v2: " + v2);
+            res.render('index', {
+                title: 'GBox2DJS Demo',
+                vars: {
+                    message: 'Welcome ' + v2.username + "!",
+                    navbar: 'GBox2DJS Demo',
+                    serverGames: gameTable
+                }
+            });
+        });
+    },
+
+    indexRedirect : function(req, res) {
+
+        res.redirect("/");
+
+    },
+
+    login : function(req,res){
+        res.render('login', {
+            title: 'GBox2DJS Demo',
+            vars: {
+                message: '',
+                navbar: 'GBox2DJS Demo'
+            }
+        });
+    },
+
+    game : function(req,res){
+
+        res.render('game', {
+            title: 'GBox2DJS Demo',
+            vars: {
+                message: '',
+                navbar: 'GBox2DJS Demo',
+                gameID: req.gameEngine.gameID
+            }
+        })
+
+    }
+
 };
